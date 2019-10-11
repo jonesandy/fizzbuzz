e278f44ab8f2222e0465eeefa91a0655659ec351
Robert <bibbycodes@gmail.com>
2019-10-11 16:23:25 +0100

    adds edge case, passes test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(number)
-  if number % 5 == 0
-    'buzz'
+  if number % 15 == 0
+    'fizzbuzz'
   elsif number % 3 == 0
     'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  elsif !(number.is_a? Integer)
+    'Error! not an integer'
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,12 +2,19 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   
-  it 'return "fizz" when passed 3' do
+  it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 
-  it 'return "buzz" when passed 5' do
+  it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "Error! not an integer" when not passed an integer' do
+    expect(fizzbuzz('5')).to eq 'Error! not an integer'
+  end
 end
